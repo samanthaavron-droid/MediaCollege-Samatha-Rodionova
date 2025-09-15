2e2efaa9104cbaec9c71299dac3051f2a872bc20
False Saint <samanthaavron@gmail.com>
2025-09-15 12:48:38 +0200

    Initial commit

---
+++ C#/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace C_;
+using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            DateTime label1 = DateTime.Now;
+            WriteLine(label1.ToString("HH:mm:ss"));
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
