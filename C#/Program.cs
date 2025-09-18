025cbe3bdf232a4567ac8e3e078c27e18e88b811
False Saint <samanthaavron@gmail.com>
2025-09-18 12:06:50 +0200

    c# lesson

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,16 +1,26 @@
 ﻿namespace C_;
-using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        while (true)
+        Console.WriteLine("What is the mark of the vechicle?");
+        string Mark = Console.ReadLine();
+        Console.WriteLine("How many gears does it have?");
+        int Gears = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("What is the frame size?");
+        double Frame = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Does it have lights? (true/false)");
+        bool Lights = Convert.ToBoolean(Console.ReadLine());
+
+        if (Lights == true)
         {
-            DateTime label1 = DateTime.Now;
-            WriteLine(label1.ToString("HH:mm:ss"));
-            Thread.Sleep(1000);
-            Console.Clear();
+            Console.WriteLine("The vechicle is a " + Mark + ", it has " + Gears + " gears, the frame size is " + Frame + " and it has lights");
         }
+        else
+        {
+            Console.WriteLine("The vechicle is a " + Mark + ", it has " + Gears + " gears, the frame size is " + Frame + " and it doesn't have lights");
+        }
+        
     }
 }
